041cbcf1e6a38eda64087b1f3b12528092e68757
Ramazan Gezer <r.gezer@reply.de>
2021-08-02 03:55:01 +0200

    take profit is active

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import json
+import requests
 import asyncio
 from fastapi import Depends, BackgroundTasks, FastAPI
 from config import Settings, get_settings
@@ -8,6 +9,7 @@ import redis_client as redis_client
 import models as models
 
 app = FastAPI()
+st_loss_profit_tasks = []
 
 
 @app.get("/")
@@ -38,8 +40,8 @@ async def place_order(signal: models.Signal, background_tasks: BackgroundTasks):
 
     if signal.side.upper() == 'SELL':
         # stop_loss_order_id = redis_client.get_from_cache(f'{pair}_stop_loss_orderId')
-        take_profit_order_id = redis_client.get_from_cache(f'{pair}_order_limit_maker_orderId')
-        await bn.cancel_order(pair, take_profit_order_id)
+        # take_profit_order_id = redis_client.get_from_cache(f'{pair}_order_limit_maker_orderId')
+        # await bn.cancel_order(pair, take_profit_order_id)
 
         quantity = utils.calculate_sell_quantity(balance, symbol_info['quantity_step_size'])
 
@@ -72,9 +74,10 @@ async def place_order(signal: models.Signal, background_tasks: BackgroundTasks):
                 # background_tasks.add_task(task_list, placed_order)
                 # background_tasks.add_task(place_stop_loss_order, signal.ticker_pair.upper(), order['price'],
                 #                          order['origQty'])
-
-                background_tasks.add_task(place_order_limit_maker, signal.ticker_pair.upper(), order['price'],
-                                          order['origQty'])
+                redis_client.delete_key(f'{signal.ticker_pair.upper()}_lslp')
+                # await task_list(order['price'], signal.ticker_pair.upper())
+                # background_tasks.add_task(stop_loss_update, order['price'], signal.ticker_pair.upper())
+                await stop_loss_update(order['price'], signal.ticker_pair.upper())
             print(order)
         # print(f'price = {price} balance = {balance}')
         return {
@@ -145,3 +148,109 @@ async def place_stop_loss_order(symbol, bought_price, quantity):
     print(stop_loss_limit)
     redis_client.set_to_cache(f'{symbol}_stop_loss_orderId', stop_loss_limit['orderId'])
     return stop_loss_limit
+
+
+@app.get("/stop/{current_price}")
+def read_root(current_price: float):
+    symbol = 'TLMBUSD'
+    bought_price = 0.2920
+    percent_var = 0.3
+    percent = float(1 - percent_var / 100)
+    stop_price = bought_price * percent
+    last_stop_price = redis_client.get_from_cache(f'{symbol}_lslp')
+    if last_stop_price is None:
+        redis_client.set_to_cache(f'{symbol}_lslp', str(stop_price))
+    win_lose_percent = calculate_current_win_lose_percent(current_price, last_stop_price, 4)
+    new_stop_price = 0.0
+    if win_lose_percent > percent_var:
+        redis_client.delete_key(f'{symbol}_lslp')
+        new_stop_price = float(current_price * percent)
+        redis_client.set_to_cache(f'{symbol}_lslp', str(new_stop_price))
+    return {
+        'last_stop_price': last_stop_price,
+        'current_price': current_price,
+        'percent_var': percent_var,
+        'percent': percent,
+        'win_lose_percent': win_lose_percent,
+        'new_stop_price': new_stop_price
+    }
+
+
+async def stop_loss_update(bought_price, symbol):
+    current_price = get_current_price(symbol)
+    percent_var = 0.5
+    percent = float(1 - percent_var / 100)
+    stop_price = float(bought_price) * float(percent)
+    last_stop_price = redis_client.get_from_cache(f'{symbol}_lslp')
+    if last_stop_price is None:
+        await place_stop_loss_order_update(symbol, stop_price)
+        redis_client.set_to_cache(f'{symbol}_lslp', str(stop_price))
+    print(f'bought price = {bought_price}')
+    win_lose_percent = calculate_current_win_lose_percent(current_price, bought_price, 4)
+    print(f'win lose percent = {win_lose_percent}')
+    new_stop_price = 0.0
+    update_percent = 0.05
+    if win_lose_percent > update_percent:
+        percent_var = 0.3
+        percent = float(1 - percent_var / 100)
+        redis_client.delete_key(f'{symbol}_lslp')
+        new_stop_price = current_price
+        stop_price = float(current_price) * float(percent)
+        await place_stop_loss_order_update(symbol, stop_price)
+        redis_client.set_to_cache(f'{symbol}_lslp', str(stop_price))
+    await asyncio.sleep(2)
+    if float(new_stop_price) > 0:
+        bought_price = new_stop_price
+    await stop_loss_update(bought_price, symbol)
+
+
+def calculate_current_win_lose_percent(current_price, bought_price, price_tick_size):
+    print(f'current_price= {current_price}, bought_price = {bought_price}, price_tick_size = {price_tick_size}')
+    bought_price = "{:0.0{}f}".format(float(bought_price), price_tick_size)
+    current_price = "{:0.0{}f}".format(float(current_price), price_tick_size)
+    # percent = float(bought_price) / float(current_price)
+    percent = round(float(((float(current_price) * 100) / float(bought_price)) - 100), 2)
+    return percent
+
+
+def get_current_price(symbol):
+    url = requests.get(f"https://api.binance.com/api/v3/ticker/price?symbol={symbol}")
+    data = url.json()
+    return data['price']
+
+
+async def place_stop_loss_order_update(symbol, stop_price):
+    settings = get_settings()
+    bn = bn_client.BinanceClientAsync(api_key=settings.api_key, secret_key=settings.api_secret)
+    stop_loss_order_id = redis_client.get_from_cache(f'{symbol}_stop_loss_orderId')
+    if stop_loss_order_id is not None:
+        # take_profit_order_id = redis_client.get_from_cache(f'{pair}_order_limit_maker_orderId')
+        await bn.cancel_order(symbol, stop_loss_order_id)
+
+    symbol_info_from_redis = redis_client.get_from_cache(f'symbol_info_{symbol}')
+    symbol_info = json.loads(symbol_info_from_redis)
+    symbol_asset = utils.extract_ticker_symbol_from_pair(symbol)
+    quantity = await bn.get_balance(symbol_asset)
+    step_size = symbol_info['quantity_step_size']
+    print(f'quantity = {quantity} step size = {step_size}')
+    quantity = utils.calculate_sell_quantity(quantity, symbol_info['quantity_step_size'])
+    price = "{:0.0{}f}".format(stop_price, symbol_info['price_tick_size'])
+    stop_price = price
+    stop_loss_limit = await bn.order_stop_loss_limit(symbol, 'SELL', quantity, stop_price, price)
+    print('waht is stop loss order')
+    print(stop_loss_limit)
+    if stop_loss_limit is not None:
+        redis_client.set_to_cache(f'{symbol}_stop_loss_orderId', stop_loss_limit['orderId'])
+    return stop_loss_limit
+
+
+async def task_list(bought_price, symbol, cancel_task=None):
+    if len(st_loss_profit_tasks) > 0:
+        for task in st_loss_profit_tasks:
+            task.cancel()
+    for i in range(1):
+        if cancel_task is not True:
+            st_loss_profit_tasks.append(asyncio.create_task(stop_loss_update(bought_price, symbol)))
+    if len(st_loss_profit_tasks) > 0:
+        await asyncio.gather(*st_loss_profit_tasks, return_exceptions=True)
+        # await asyncio.gather(*st_loss_profit_tasks)
